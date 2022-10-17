bebfd116fa3e2406bf1341fd6a590cfda7554275
Skaleto <sskaleto61@gmail.com>
2022-10-17 17:20:00 +0800

    fix task

--- Home_2/JS/photo_zoom.js
+++ Home_2/JS/photo_zoom.js
@@ -1,12 +1,6 @@
-var animTime = 1500;
-$('.photo').hover(function(e){
-    var modal = $('#zoom_shirt');
-    modal.css('top', (window.innerHeight - modal.height()) / 2);
-    modal.css('left', (window.innerWidth - modal.width()) / 2);
-    modal.css('background-image', $(e.target).css('background-image'));
-    modal.fadeIn(animTime);
-})
-
-$('#zoom_shirt').on('click', function (){
-    $(this).fadeOut(animTime);
+let animTime = 1500;
+let zoomImg = document.getElementById("zoom_photo");
+$('.photo_shirt').hover(function() {
+    let src_photo_shirt = $(this).attr('src');
+    zoomImg.setAttribute('src', src_photo_shirt);
 })
